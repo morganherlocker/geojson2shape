13e83dd8c1733373564ed9d9d81a4a71b948e845
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-19 10:32:28 -0500

    implemented zip functionality

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 var exec = require('child_process').exec,
-    path = require('path')
+    path = require('path'),
+    archiver = require('archiver'),
+    fs = require('fs')
 
-module.exports = function(inPath, outPath, done){
+module.exports = function(inPath, outPath, zip, done){
   var ogrCommand = 'ogr2ogr -f "ESRI Shapefile" '+path.resolve(process.cwd()+'/'+outPath)+ ' '+
                     path.resolve(process.cwd()+'/'+inPath)+ ' '+
                     '-overwrite'
 
   exec(ogrCommand, function(err, stdout, stderr){
-    done(err)
+    if(zip){
+      var output = fs.createWriteStream(path.resolve(process.cwd()+'/'+outPath).slice(0, -4) + '.zip')
+      var archive = archiver('zip');
+      archive.on('error', function(err) {
+        done(err);
+      });
+
+      archive.pipe(output);
+      var file1 = path.resolve(process.cwd()+'/'+outPath).slice(0, -4) + '.dbf'
+      var file2 = path.resolve(process.cwd()+'/'+outPath).slice(0, -4) + '.prj'
+      var file3 = path.resolve(process.cwd()+'/'+outPath).slice(0, -4) + '.shp'
+      var file4 = path.resolve(process.cwd()+'/'+outPath).slice(0, -4) + '.shx'
+      archive
+        .append(fs.createReadStream(file1), { name: path.basename(file1) })
+        .append(fs.createReadStream(file2), { name: path.basename(file2) })
+        .append(fs.createReadStream(file3), { name: path.basename(file3) })
+        .append(fs.createReadStream(file4), { name: path.basename(file4) });
+
+      archive.finalize(function(err, bytes) {
+        done(err)
+      })
+    }
+    else{
+      done(err)
+    }
   })
 }
--- test/geojson2shape.js
+++ test/geojson2shape.js
@@ -3,7 +3,13 @@ var geojson2shape = require('../index.js'),
 
 describe('geojson2shape', function(){
   it('should take a geojson file and output a shapefile', function(done){
-    geojson2shape('./in.geojson', './out.shp', function(err){
+    geojson2shape('./in.geojson', './out.shp', false, function(err){
+      if(err) throw err
+      done()
+    })
+  })
+  it('should take a geojson file and output a shapefile zipped', function(done){
+    geojson2shape('./in.geojson', './out.shp', true, function(err){
       if(err) throw err
       done()
     })
